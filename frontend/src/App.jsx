bc91164ce77d407cfefb3cc5e266f1d9f84c7005
Musab Hassan <musabhassan1999@gmail.com>
2024-03-06 16:45:19 +0530

    understanding Keys

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import "./App.css";
-import ButtonWithTitle from "./componenets/experiment/ButtonWithTitle";
-import Header from "./componenets/experiment/Header";
+import Todo from "./componenets/todo/Todo";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Todo1", discription: "Todo1 discription" },
+    { id: 2, title: "Todo2", discription: "Todo2 discription" },
+    { id: 3, title: "Todo3", discription: "Todo3 discription" },
+  ]);
+
+  function handleTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: 4,
+        title: "Todo4",
+        discription: "Todo4 discription",
+      },
+    ]);
+  }
+
   return (
     <div>
       <h1>Musab Hassan</h1>
       <h3>Full Stack Developer</h3>
-      <ButtonWithTitle />
-      <Header title="I am Title" />
+      <button onClick={handleTodo}>Add Todo</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} discription={todo.discription} />
+      ))}
     </div>
   );
 }
---
+++ frontend/src/componenets/todo/Todo.jsx
@@ -0,0 +1,10 @@
+export default function Todo({ title, discription }) {
+  return (
+    <>
+      <div>
+        <h2>{title}</h2>
+        <h4>{discription}</h4>
+      </div>
+    </>
+  );
+}
