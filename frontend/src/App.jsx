1dfffdd1b068c14135c99d1dd9a289e3d986402d
Musab Hassan <musabhassan1999@gmail.com>
2024-03-07 13:53:44 +0530

    learn UseEffect()

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import Todo from "./componenets/todo/Todo";
+import TodoWithId from "./componenets/experiment/TodoWithId";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, title: "Todo1", discription: "Todo1 discription" },
-    { id: 2, title: "Todo2", discription: "Todo2 discription" },
-    { id: 3, title: "Todo3", discription: "Todo3 discription" },
-  ]);
-
-  function handleTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: 4,
-        title: "Todo4",
-        discription: "Todo4 discription",
-      },
-    ]);
-  }
-
   return (
     <div>
       <h1>Musab Hassan</h1>
       <h3>Full Stack Developer</h3>
-      <button onClick={handleTodo}>Add Todo</button>
-      {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} discription={todo.discription} />
-      ))}
+      <TodoWithId id={5} />
     </div>
   );
 }
---
+++ frontend/src/componenets/experiment/TodoWithId.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function TodoWithId({ id }) {
+  const [todo, setTodo] = useState({});
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?id=" + id).then((res) => {
+      setTodo(res.data.todo);
+    });
+  }, []);
+  return (
+    <>
+      <h1>{todo.title}</h1>
+      <h4>{todo.description}</h4>
+    </>
+  );
+}
